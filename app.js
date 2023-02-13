1e1615752e9cc4b7d3fde1dcedc207394e9cc5fa
Celeste Rhoades <celesterhoades@celestes-MacBook-Pro.local>
2023-02-12 19:38:42 -0700

    Ok final Chinese Zodiac

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const chineseZodiac = () => {
     const zodiacAnimal = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep", "Monkey", "Rooster", "Dog", "Pig"];
 
     const zodiacSaying = ["You are lucky with money", "You are easy going, and inspire others", "You are very successful", "You are clever and wise", "You are honest, sensitive and brave", "You say little, but have great wisdom", "You are cheerful, skillful with money, and perceptive", "You are shy, pessimistic, and puzzled about life", "You are  Clever, skillful, and flexible", "You are a deep thinker, capable, and talented", "You have a deep sense of loyalty, are honest, and can keep secrets", "Studious and gallant with tremendous inner strength"];
-    return `Hi your sign is a ${zodiacAnimal[random(zodiacAnimal)]} which means ${zodiacSaying[random(zodiacSaying)]}`
+    return `Hi your sign is a ${zodiacAnimal[random(zodiacAnimal)]}! which means ${zodiacSaying[random(zodiacSaying)]}`
 }
 console.log(chineseZodiac());
